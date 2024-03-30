71418a9a3177b34c3387de5f44e96def3ba24e41
mitches-got-glitches <mitch.edmunds@proton.me>
2024-03-30 16:25:07 +0000

    Make python install check for positional argument

--- install_python_versions.sh
+++ install_python_versions.sh
@@ -8,7 +8,7 @@ show_help() {
   echo "Usage: "$(basename "$0")" [-h] n"
   echo "Options:"
   echo "  -h, --help     Display this help message"
-  # Add more options and descriptions as needed
+  echo "  n              Number of minor versions to install"
   exit 0
 }
 
@@ -20,10 +20,13 @@ while getopts "h" opt; do
   esac
 done
 
-############################################################
-# Main program                                             #
+# Replace default with first posarg.
+if ! [ -z "$1" ]; then
+  n_vers=$1
+fi
+
 # Install the latest patch for last n minor Python versions.
-py_versions=$(pyenv install --list | grep -v '[a-z]' | grep -Po '\d+\.\d+' | uniq | tail -$n_vers)
+py_versions=$(pyenv install --list | grep -v '[a-z]' | grep -Po '\d+\.\d+' | uniq | tail -$n_vers | tac)
 for ver in $py_versions; do pyenv install $(pyenv install --list | grep $ver | grep -v '[a-z]' | tail -1); done
 # Set the global Python as the most recent
 latest_py=$(pyenv versions | grep -Po '\d+\.\d+\.\d+' | tail -1)
