6a8bc0e85697db6581db72e05cc8b2fe2fd2e7dd
mitches-got-glitches <mitch.edmunds@proton.me>
2024-03-30 14:59:38 +0000

    Update help on install_python_versions.sh

--- install_python_versions.sh
+++ install_python_versions.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-############################################################
-# Help                                                     #
-############################################################
+# Default value
+n_vers=1
+
 show_help() {
   echo "Install the latest patch for last n minor Python versions."
   echo
-  echo "Usage: $(install_python_versions $0) [-h]"
+  echo "Usage: "$(basename "$0")" [-h] n"
   echo "Options:"
   echo "  -h, --help     Display this help message"
   # Add more options and descriptions as needed
   exit 0
 }
 
-if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-  show_help
-fi
+# Parse command-line options
+while getopts "h" opt; do
+  case $opt in
+    h) show_help ;;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+  esac
+done
+
 ############################################################
 # Main program                                             #
 # Install the latest patch for last n minor Python versions.
-py_versions=$(pyenv install --list | grep -v '[a-z]' | grep -Po '\d+\.\d+' | uniq | tail -$1)
+py_versions=$(pyenv install --list | grep -v '[a-z]' | grep -Po '\d+\.\d+' | uniq | tail -$n_vers)
 for ver in $py_versions; do pyenv install $(pyenv install --list | grep $ver | grep -v '[a-z]' | tail -1); done
 # Set the global Python as the most recent
 latest_py=$(pyenv versions | grep -Po '\d+\.\d+\.\d+' | tail -1)
