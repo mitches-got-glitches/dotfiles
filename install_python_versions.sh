e5edd06c1269cb1913d5164d00c2ffc2ef492dd8
mitches-got-glitches <mitch.edmunds@proton.me>
2024-03-30 14:36:42 +0000

    Make executable and improve help

--- install_python_versions.sh
+++ install_python_versions.sh
@@ -2,20 +2,22 @@
 ############################################################
 # Help                                                     #
 ############################################################
-Help()
-{
-   # Display Help
-   echo "Install the latest patch for last n minor Python versions."
-   echo
-   echo "Syntax: install_python_versions.sh $1"
-   echo "One positional arg for n versions"
+show_help() {
+  echo "Install the latest patch for last n minor Python versions."
+  echo
+  echo "Usage: $(install_python_versions $0) [-h]"
+  echo "Options:"
+  echo "  -h, --help     Display this help message"
+  # Add more options and descriptions as needed
+  exit 0
 }
 
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  show_help
+fi
 ############################################################
 # Main program                                             #
-
-Help
-# Install the latest patch for last x minor Python versions.
+# Install the latest patch for last n minor Python versions.
 py_versions=$(pyenv install --list | grep -v '[a-z]' | grep -Po '\d+\.\d+' | uniq | tail -$1)
 for ver in $py_versions; do pyenv install $(pyenv install --list | grep $ver | grep -v '[a-z]' | tail -1); done
 # Set the global Python as the most recent
